c52a558e6eab0961d7456cebef9ae23658f7ff54
jungjisu <jungjisu0809@gmail.com>
2023-04-13 17:45:39 +0900

    배열문 연습

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,37 +2,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] a = new int[5]; // 5개의 정수를 저장할 배열 생성
+        Scanner s = new Scanner(System.in);
+        int[] a = new int[5]; // 배열문의 5개의 정수 입력 받음
 
-        // 5개의 정수 입력 받기
-        for (int i = 0; i < 5; i++) {
-            System.out.print((i+1) + "번째 정수 입력 : ");
-            a[i] = scanner.nextInt();
+        for (int i = 0; i < 5; i++) { // i가 0으로 저장, i가 5보다 작을떄 다음행으로 너어감
+            System.out.print((i+1) + "번째 정수 입력 : "); // 사용자가 정수나 실수 입력하기.
+            a[i] = s.nextInt(); // 값을 입력받음
         }
+        int max = a[0]; //최대값을 0
+        int min = a[0]; // 최소값을 0
+        int sum = 0; // 평균값을 0
 
-        int max = a[0]; // 배열의 첫 번째 원소를 최대값으로 초기화
-        int min = a[0]; // 배열의 첫 번째 원소를 최소값으로 초기화
-        int sum = 0; // 입력 받은 정수들의 합을 저장할 변수 초기화
-
-        // 최대값, 최소값, 합 구하기
-        for (int i = 0; i < 5; i++) {
-            if (max < a[i]) {
-                max = a[i];
-            }
-            if (min > a[i]) {
-                min = a[i];
-            }
-            sum += a[i];
+        for (int i = 0; i < 5; i++) {  // i를 1로 저장 i가 5보다 작을떄 다음행으로
+            if (max < a[i]) max = a[i]; //최대값이 i 배열문의 값보다 작으면 max는 그 i값으로 저장함.
+            if (min > a[i]) min = a[i]; // 최소값이 i 배열문의 값보다 크면 min는 그 i값으로 저장함.
+            sum += a[i]; // 평균값은 sum에 i를 더한 값을 대입한다
         }
+        float avg = (float) sum / 5;
 
-        double avg = (double) sum / 5; // 평균 구하기
-
-        // 결과 출력
         System.out.println("가장 큰 값 : " + max);
         System.out.println("가장 작은 값 : " + min);
         System.out.println("평균 값 : " + avg);
 
-        scanner.close();
+
     }
 }
+
