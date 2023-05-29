d0a84cae56d1c8bf1eb434d7425b7e1b7cc81bfd
jungjisu <jungjisu0809@gmail.com>
2023-05-29 17:26:11 +0900

    연도와 월을 입력받아 해당 월의 날수를 출력하다가 월이 0이면 종료함

---
+++ src/main/java/JavaPRJ/PRJ22.java
@@ -0,0 +1,63 @@
+package JavaPRJ;
+
+import java.util.*;
+
+public class PRJ22 {
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+
+        int[] day = new int[13];
+        int i, year;
+
+        for( i=1; i<day.length; i++)
+        {
+            if( i==2)
+            {
+                day[i] = 28;
+            }
+            else if( i==4 || i==6 || i==9 || i==11)
+            {
+                day[i] = 30;
+            }
+            else
+            {
+                day[i] = 31;
+            }
+        }
+
+        while(true)
+        {
+            System.out.print("년도 : ");
+            year = scn.nextInt();
+            System.out.print("월 : ");
+            i = scn.nextInt();
+
+            if(i == 0)
+            {
+                break;
+            }
+
+            if(i<1 || i>12)
+            {
+                System.out.print("잘못 입력하였습니다.\n\n");
+                continue;
+            }
+
+            else if( i==2)
+            {
+                if(year%400==0||((year%4==0)&&(year%100!=0)))
+                {
+                    day[2] = 29;
+                }
+                else
+                {
+                    day[2] = 28;
+                }
+            }
+
+            System.out.printf("입력하신 달의 날수는 %d일입니다.\n\n", day[i]);
+        }
+    }
+}
+
